d9ce768cf2f4c55a0630fb69d7e674deb5b77bde
Xuan Vinh <19110011@student.hcmute.edu.vn>
2023-10-11 11:00:03 +0700

    modify cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -6,8 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { UpdateError, UpdateSuccessNavigate, UpdateSuccessReload } from '../../components/alert';
 import { useNavigate } from 'react-router-dom';
 import { getUserFromLocalStorage } from '../../utils/userHandle';
-import { Image,ButtonGroup, Button, Input } from "@nextui-org/react";
 import IconDeleteItem from '../../components/icons/IconDeleteItem';
+import { Skeleton } from '@mui/material';
 
 
 export default function Cart() {
@@ -78,70 +78,40 @@ export default function Cart() {
     };
     return (
         <div>
-            <div className=' bg-white w-10/12 rounded-lg mx-auto my-10 h-16 drop-shadow-lg flex justify-center items-center'>
-                <p className='text-yellow-600 text-xl font-semibold'>GIỎ HÀNG CỦA BẠN</p>
-            </div>
+            <div>
+                <div className=' bg-white w-10/12 rounded-lg mx-auto my-10 h-16 drop-shadow-lg flex justify-center items-center'>
+                    <p className='text-yellow-600 text-xl font-semibold'>GIỎ HÀNG CỦA BẠN</p>
+                </div>
 
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-10/12 mx-auto my-4 h-[400px]">
-                        <table className="table-auto w-full text-base text-left text-gray-500 border-collapse border border-gray-500">
-                            <thead className=" text-xs text-gray-700 uppercase bg-gray-50 ">
-                                <tr>
-                                    <th scope="col" className="px-6 py-3 border border-gray-500">
-                                        Sản phẩm
-                                    </th>
-                                    <th scope="col" className="px-6 py-3 border border-gray-500">
-                                        Hình ảnh
-                                    </th>
-                                    <th scope="col" className="px-6 py-3 border border-gray-500">
-                                        Giá tiền
-                                    </th>
-                                    <th scope="col" className="px-6 py-3 border border-gray-500">
-                                        Size
-                                    </th>
-                                    <th scope="col" className="px-6 py-3 border border-gray-500">
-                                        Số lượng
-                                    </th>
-                                    <th scope="col" className="px-6 py-3 border border-gray-500">
-                                        Hủy
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {cart?.items?.map((cartItem) => (
-                                <tr className="bg-white border-b ">
-                                    <td scope="row" className=" px-6 py-4 font-medium  text-gray-900  dark:text-white border border-gray-500">
-                                       <div className='w-64'>{cartItem.name}</div>
-                                    </td>
-                                    <td scope="row" className="px-6 py-4 border border-gray-500">
-                                       <img src={cartItem.image} alt="product-image" className='w-[100px] h-[100px]' />
-                                    </td>
-                                    <td scope="row" className="px-6 py-4 border border-gray-500">
-                                        {formatPrice(cartItem.price * cartItem.quantity)}
-                                    </td>
-                                    <td scope="row" className="px-6 py-4 border border-gray-500">
-                                        {cartItem.size}
-                                    </td>
-                                    <td scope="row" className="px-6 py-4 border border-gray-500">
-                                      
-                                        <ButtonGroup>
-                                            <Button
-                                                clickable
-                                                className='bg-yellow-600'
-                                                size='sm'
-                                                onPress={() => updateChangeCart(cartItem.productOptionId, cartItem.color, -1)}
-                                            >
-                                                -
-                                            </Button>
-                                            <Input
-                                                isReadOnly
-                                                size='sm'
-                                                type={'number'}
-                                                shadow={false}
-                                                underlined
-                                                radius='none'
-                                                className='w-14'
+                <div className='w-10/12 mx-auto text-lg font-semibold'>{cart?.totalProduct || 0}&nbsp;Sản phẩm</div>
+
+                <div className="w-10/12 mx-auto flex gap-10">
+                    <div className="rounded-lg w-2/3">
+
+                        {cart?.items?.map((cartItem) => (
+                            <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                                <img src={cartItem.image} alt="product-image" className="w-full rounded-lg sm:w-40" />
+                                <div className="sm:ml-4 flex sm:w-full sm:justify-between">
+                                    <div className="mt-5 sm:mt-0">
+                                        <h2 className="text-lg font-bold text-gray-900">{cartItem.name}</h2>
+                                        <p className="mt-1 text-sm text-gray-700">Size:&nbsp;{cartItem.size}</p>
+                                        <div className='flex gap-4'>
+                                            <p className='mt-1 text-lg text-red-700 font-bold'>{formatPrice(cartItem.subPrice)}</p>
+                                            <del className='mt-1 text-lg text-gray-500 font-bold'> {formatPrice(cartItem.price * cartItem.quantity)}</del>
+                                        </div>
+                                        <button onClick={() => handleRemoveCartItem(cartItem.itemId)} className="mt-14 space-x-4 text-red-600">       
+                                            <IconDeleteItem></IconDeleteItem>
+                                        </button>       
+                                    </div>
+                                    <div className="mt-4 sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                                        <div className="flex items-center border-gray-100">
+                                            <button onClick={() => updateChangeCart(cartItem.productOptionId, cartItem.color, -1)} className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </button>
+                                            <input readOnly
+                                                className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                                                type="number"
                                                 value={cartItem.quantity}
                                                 defaultValue={cartItem.quantity}
+                                                min="1"
                                                 onBlur={(e) => {
                                                     if (e.target.value <= 0) {
                                                         toast.error('Số lượng không hợp lệ. Vui lòng nhập lại', {
@@ -159,37 +129,51 @@ export default function Cart() {
                                                             cartItem.color,
                                                             e.target.value - cartItem.quantity,
                                                         )
-                                                        } 
                                                     }
                                                 }
+                                                }
                                             />
-                                            <Button
-                                                clickable
-                                                size='sm'
-                                                className='bg-yellow-600'
-                                                onPress={() => updateChangeCart(cartItem.productOptionId, cartItem.color, 1)}
-                                            >
-                                                +
-                                            </Button>
-                                            </ButtonGroup>
-                                        
-                                    </td>
-                                    <td className="px-6 py-4 border border-gray-500">
-                                        <button type='button' onClick={() => handleRemoveCartItem(cartItem.itemId)} className="font-medium text-red-600 hover:underline"><IconDeleteItem></IconDeleteItem></button>
-                                    </td>
-                                    
-                                </tr>
-                            ))}
+                                            <button onClick={() => updateChangeCart(cartItem.productOptionId, cartItem.color, 1)} className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </button>
+                                        </div>
+                                         
+                                    </div>
 
-                            </tbody>
-                        </table>
-                   
+                                </div>
+                            </div>
+                        ))}
 
-            </div>
-            <div className='flex justify-center mb-10'>
-                <button type='button' disabled={cart === '404'} onClick={handleClickOrder} className="w-30 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-yellow-700 rounded-md hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600">
-                    Đặt hàng
-                </button>
+                    </div>
+                    {/* <!-- Sub total --> */}
+                    {cart === '404' ? <></> :
+                    <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 w-1/3">
+                        {cart?.totalPrice !== undefined ? ( <>
+                        <div className="mb-2 flex justify-between">
+                            <p className="text-gray-700">Tổng số tiền</p>
+                            <p className="text-gray-700">{formatPrice(cart?.items.reduce((total, cur) => total + (cur.price * cur.quantity), 0)) || 0}</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p className="text-gray-700">Giảm giá</p>
+                            <p className="text-gray-700">{formatPrice(cart?.items.reduce((total, cur) => total + (cur.price * cur.quantity), 0) - cart?.totalPrice) || 0}</p>
+                        </div>
+                        <hr className="my-4" />
+                        <div className="flex justify-between">
+                            <p className="text-lg font-bold">Thanh toán</p>
+                            <div className="">
+                                <p className="mb-1 text-lg font-bold"> {formatPrice(cart?.totalPrice || 0)}</p>
+                            </div>
+                        </div>
+                        <button disabled={cart === '404'} onClick={handleClickOrder} className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Đặt hàng</button>
+                        </>
+                        ) : (
+                            Array.from(new Array(5)).map((index) => (
+                                <div key={index} css={{ marginTop: '$10' }}>
+                                    <Skeleton width={'100%'} /> 
+                                </div>
+                            ))
+                        )}
+                    </div>
+                    }
+                </div>
             </div>
             <ToastContainer></ToastContainer>
         </div>
