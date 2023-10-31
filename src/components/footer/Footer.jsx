7ce577c71ec7dab01307249989ec2491718ab3b5
Xuan Vinh <19110011@student.hcmute.edu.vn>
2023-10-31 15:07:34 +0700

    update reponsive footer,header

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,62 +4,98 @@ import IconPhone from '../icons/IconPhone'
 
 export default function Footer() {
   return (
-    <div>
-      <div className='bottom-0 bg-slate-100 h-60 flex flex-row justify-around pt-4 '>
+      <div className="flex flex-col items-center bg-gray-200 text-center  lg:text-left">
+        <div className="container p-6">
+          <div className="grid place-items-center md:grid-cols-2 lg:grid-cols-3">
 
-        <div>
-          <div className='text-xs font-bold'>ĐỊA CHỈ</div>
+            <div className="mb-6">
+              <h5
+                className="mb-2.5 font-bold uppercase text-neutral-800 ">
+                ĐỊA CHỈ
+              </h5>
+              <ul className="mb-0 list-none">
+                <li className='flex gap-2'>
+                  <IconLocation></IconLocation>
+                  <a href="#!" className="text-neutral-800 ">73 Lý Tự Trọng, Quận 1, Tp. HCM</a>
+                </li>
+                <li className='flex gap-2'>
+                  <IconLocation></IconLocation>
+                  <a href="#!" className="text-neutral-800 ">13 Nguyễn Thiện Thuật, Quận 3, TP. HCM</a>
+                </li>
+                <li className='flex gap-2'>
+                  <IconLocation></IconLocation>
+                  <a href="#!" className="text-neutral-800 ">210B Hồ Văn Huê, Quận Phú Nhuận, TP. HCM</a>
+                </li>
+                <li className='flex gap-2'>
+                  <IconLocation></IconLocation>
+                  <a href="#!" className="text-neutral-800 ">261 Phố Huế, Quận Hai Bà Trưng, Hà Nội</a>
+                </li>
+                <li className='flex gap-2'>
+                  <IconPhone></IconPhone>
+                  <a href="#!" className="text-neutral-800 ">0898 515 689</a>
+                </li>
+                <li className='flex gap-2'>
+                  <a href="#!" className="text-neutral-800 ">Thời gian làm việc: Thứ Hai - Thứ Bảy 8h-17h</a>
+                </li>
+              </ul>
+            </div>
 
-          <div className='flex my-2'>
-            <IconLocation></IconLocation>
-            <div className='ml-2'> 73 Lý Tự Trọng, Quận 1, Tp. HCM</div>
-          </div>
-          <div className='flex my-2'>
-            <IconLocation></IconLocation>
-            <div className='ml-2'> 13 Nguyễn Thiện Thuật, Quận 3, TP. HCM</div>
-          </div>
-          <div className='flex my-2'>
-            <IconLocation></IconLocation>
-            <div className='ml-2 '> 210B Hồ Văn Huê, Quận Phú Nhuận, TP. HCM</div>
-          </div>
-          <div className='flex my-2'>
-            <IconLocation></IconLocation>
-            <div className='ml-2 '> 261 Phố Huế, Quận Hai Bà Trưng, Hà Nội</div>
-          </div>
-          <div className='flex my-2'>
-            <IconPhone></IconPhone>
-            <div className='ml-2'> 0898 515 689</div>
-          </div>
 
-          <div> Thời gian làm việc: Thứ Hai - Thứ Bảy 8h-17h</div>
+            <div className="mb-6">
+              <h5
+                className="mb-2.5 font-bold uppercase text-neutral-800 ">
+                HƯỚNG DẪN
+              </h5>
+
+              <ul className="mb-0 list-none">
+                <li>
+                  <a href="#!" className="text-neutral-800 ">HƯỚNG DẪN MUA HÀNG</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">GIAO NHẬN VÀ THANH TOÁN</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">ĐỔI TRẢ VÀ BẢO HÀNH</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">TRA CỨU ĐƠN HÀNG</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">ĐĂNG KÍ THÀNH VIÊN</a>
+                </li>
+              </ul>
+            </div>
+
+
+            <div className="mb-6">
+              <h5
+                className="mb-2.5 font-bold uppercase text-neutral-800 ">
+                CHÍNH SÁCH
+              </h5>
+
+              <ul className="mb-0 list-none">
+                <li>
+                  <a href="#!" className="text-neutral-800 ">CHĂM SÓC KHÁCH HÀNG</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">CHÍNH SÁCH ĐỔI HÀNG</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800">CHÍNH SÁCH BẢO HÀNH</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">CHÍNH SÁCH THANH TOÁN</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-neutral-800 ">CHÍNH SÁCH VẬN CHUYỂN</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-
-        <div>
-          <div className='text-xs font-bold'>HƯỚNG DẪN</div>
-          <div className='my-2'>HƯỚNG DẪN MUA HÀNG</div>
-          <div className='my-2'>GIAO NHẬN VÀ THANH TOÁN</div>
-          <div className='my-2'>ĐỔI TRẢ VÀ BẢO HÀNH</div>
-          <div className='my-2'>TRA CỨU ĐƠN HÀNG</div>
-          <div className='my-2'>ĐĂNG KÍ THÀNH VIÊN</div>
-          <div></div>
+        <div className="w-full bg-gray-400 p-4 text-center text-neutral-700 ">
+          © 2010 - 2023 Fashion Shoes
         </div>
-
-        <div>
-          <div className='text-xs font-bold'>CHÍNH SÁCH</div>
-          <div className='my-2'>CHĂM SÓC KHÁCH HÀNG</div>
-          <div className='my-2'>HỆ THỐNG TÍCH ĐIỂM</div>
-          <div className='my-2'>CHÍNH SÁCH ĐỔI HÀNG</div>
-          <div className='my-2'>CHÍNH SÁCH BẢO HÀNH</div>
-          <div className='my-2'>CHÍNH SÁCH THANH TOÁN</div>
-          <div className='my-2'>CHÍNH SÁCH VẬN CHUYỂN</div>
-        </div>
-
-
       </div>
-      <div className="bg-slate-200 flex justify-center">
-        <hr className="light-100" />
-        <h5>© 2010 - 2023 Fashion Shoes </h5>
-      </div>
-    </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,13 +9,16 @@ import Categories from './components/Categories'
 import { Tooltip } from '@nextui-org/react'
 import { getAllCategory } from '../../api/CategoryApi'
 import { KeyboardArrowDown } from '@mui/icons-material'
+import IconMenu from '../icons/IconMenu'
+import IconCart from '../icons/IconCart'
 export default function Header() {
-  
+
   let userCurrent = getUserFromLocalStorage()
   let navigate = useNavigate();
   const [isOpen, setOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
   const dispatch = useDispatch();
-  
+
   const [categories, setCategories] = useState([])
   useEffect(() => {
     async function getData() {
@@ -28,6 +31,9 @@ export default function Header() {
 
   const handleDropDown = () => {
     setOpen(!isOpen);
+  };
+  const handleDropDownMenu = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   const [search, setSearch] = useState('')
@@ -45,13 +51,13 @@ export default function Header() {
     dispatch(authAction.logout())
   }
   return (
-    <div className='w-full' >
+    <div >
       <div className='flex items-center justify-end pr-6 py-2'>
         <Link to='/' className='pr-2 hover:text-yellow-700'>👑 SneakerHead</Link>
         {userCurrent?.id !== undefined ? <>
           <div className="dropdown" >
             <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" onClick={handleDropDown} className="border-l-[1px] pl-2 border-gray-300 focus:outline-none text-center inline-flex items-center " type="button">Xin chào&nbsp;,&nbsp;<span className='font-semibold'>{userCurrent?.name}</span>
-            <IconDropDown></IconDropDown>
+              <IconDropDown></IconDropDown>
             </button>
 
             <div
@@ -62,19 +68,19 @@ export default function Header() {
               <ul className=" z-10 w-44 bg-white rounded shadow ">
                 <li
                 >
-                  <Link to='order' onClick={handleDropDown}className="block py-2 px-4 hover:bg-gray-100 w-full text-left">
+                  <Link to='order' onClick={handleDropDown} className="block py-2 px-4 hover:bg-gray-100 w-full text-left">
                     Đơn hàng của tôi
                   </Link>
                 </li>
                 <li
                 >
-                  <Link to={`/profile/${userCurrent.id}`} onClick={handleDropDown}className="block py-2 px-4 hover:bg-gray-100 w-full text-left">
+                  <Link to={`/profile/${userCurrent.id}`} onClick={handleDropDown} className="block py-2 px-4 hover:bg-gray-100 w-full text-left">
                     Thông tin cá nhân
                   </Link>
                 </li>
                 <li
                 >
-                  <button type='button' onClick={handleLogout}className="block py-2 px-4 hover:bg-gray-100 w-full text-left">
+                  <button type='button' onClick={handleLogout} className="block py-2 px-4 hover:bg-gray-100 w-full text-left">
                     Đăng xuất
                   </button>
                 </li>
@@ -82,50 +88,77 @@ export default function Header() {
             </div>
           </div>
         </> : <>
-            <Link to='/login' className='border-l-[1px] border-gray-300 px-2 hover:text-yellow-700'>Đăng nhập</Link>
-            <Link to='/register' className='border-l-[1px] border-gray-300 px-2 hover:text-yellow-700'>Đăng ký</Link>
-          </>
+          <Link to='/login' className='border-l-[1px] border-gray-300 px-2 hover:text-yellow-700'>Đăng nhập</Link>
+          <Link to='/register' className='border-l-[1px] border-gray-300 px-2 hover:text-yellow-700'>Đăng ký</Link>
+        </>
         }
       </div>
 
-      <div className='flex items-center px-7 h-24 gap-8'>
-        <img src={require('../../assets/ShoesLogo.png')} alt="ShoesLogo" style={{ width: '80px', height: '80px' }} />
-        <div>
-          <div className=' font-bold'>SNEAKERHEAD SHOP</div>
-          <div className=' font-bold text-xs'>Walk in style with fashion shoes.</div>
+     
+      <nav
+        className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow-lg border-solid mb-5">
+        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+          <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
+            <img src={require('../../assets/ShoesLogo.png')} alt="ShoesLogo" style={{ width: '40px', height: '40px' }} />
+            <span className="font-semibold text-xl tracking-tight">&nbsp;&nbsp;SNEAKER SHOES</span>
+          </div>
+          <div className="block lg:hidden ">
+            <div className='flex gap-4'>
+              <button
+                onClick={handleDropDownMenu}
+                id="nav"
+                className="flex items-center px-3 py-2 border-2 rounded text-yellow-700 border-yellow-700 hover:text-yellow-700 hover:border-yellow-700">
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+              <div className='mt-1'>
+                <IconCart></IconCart>
+              </div>
+            </div>
+          </div>
         </div>
-        <Link to='/' className='font-bold hover:text-yellow-700'>TRANG CHỦ</Link>
-        <Tooltip
+
+        <div className={`menu w-full flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8 ${isMenuOpen ? "block" : "hidden"
+                }`}>
+          <div className="text-md font-bold  lg:flex-grow">
+            <Link to='/'
+              className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-2 py-2 rounded hover:bg-yellow-700 mr-2">
+              TRANG CHỦ
+            </Link>
+            <Tooltip
               content={<Categories categories={categories}/>}
               css={{ left: '$0', transform: 'none' }}
               placement='bottom'
-              
                >
-        <p 
-        className='font-bold hover:text-yellow-700 cursor-default flex forcus:outline-0 outline-0'>
-        SẢN PHẨM <KeyboardArrowDown/> 
-        </p>
-        </Tooltip>
-        <Link to='/cart' className='font-bold hover:text-yellow-700'>GIỎ HÀNG</Link>
-        <Link to='/order' className='font-bold hover:text-yellow-700'>ĐƠN HÀNG</Link>
-        <Link to='/' className='font-bold hover:text-yellow-700'>CHÍNH SÁCH</Link>
+            <p className="block cursor-default mt-4 lg:inline-block lg:mt-0 hover:text-white px-2 py-2 rounded hover:bg-yellow-700 mr-2">
+              SẢN PHẨM
+            </p>
+            </Tooltip>
+            <Link to='/order'
+              className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-2 py-2 rounded hover:bg-yellow-700 mr-2">
+              ĐƠN HÀNG
+            </Link>
+            <Link to='/'
+              className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-2 py-2 rounded hover:bg-yellow-700 mr-2">
+              CHÍNH SÁCH
+            </Link>
+          </div>
 
-        <form className='ml-auto'>
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-              </svg>
-            </div>
-            <input type="search" id="default-search" value={search} onChange={handleSearchChange} className="focus:outline-none block w-[300px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-yellow-500 focus:border-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500" placeholder="Tìm kiếm sản phẩm..." required />
-            <button type="button" onClick={handleSearch} className="text-white absolute right-2.5 bottom-2.5 bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+          <div className="relative mx-auto text-gray-600 lg:block hidden">
+            <input
+              className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
+              type="search" name="search" placeholder="Search"
+              value={search} onChange={handleSearchChange} />
+            <button type="button" onClick={handleSearch} className="absolute right-0 top-0 mt-3 mr-2">
               <IconSearch></IconSearch>
             </button>
           </div>
-        </form>
-
-      </div>
+          <Link to='/cart' className='ml-4 lg:block hidden'>
+            <IconCart></IconCart>
+          </Link>
+        </div>
+      </nav>
     </div>
   )
 }
